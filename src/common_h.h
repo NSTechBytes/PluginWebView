8310792f282a2dae51e96996cc41dd138fe0fa5b
nstechbytes <nstechbytes@gmail.com>
2025-11-15 19:27:48 +0500

    Add StringUtil and update project configuration

--- src/common_h.h
+++ src/common_h.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <Windows.h>
 
+// Include our local fixed StringUtil.h BEFORE Rainmeter headers
+// to prevent the Rainmeter version from being included
+#include "StringUtil.h"
+
 #include "Plugins/API/RainmeterAPI.h"
 
 #include <wrl.h>
